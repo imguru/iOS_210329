acdf58420f68d3232341d223cbd2ffbc31875eb0
Chansik Yun <chansigi@gmail.com>
2021-03-29 14:36:15 +0900

    .

---
+++ SwiftSample/SwiftSample/main.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+// Optional / Nullable
+//  => nil을 안전하게 다루는 기능
+struct User {
+  let id: Int
+  let email: String
+  
+  let name: String?  // Optional<String>
+  // let name: Optional<String>
+}
+
+let user = User(id: 0, email: "hello@gmail.com", name: "Tom")
+
+// 1) enum을 통해서 Optional이 구현되어 있습니다.
+/*
+enum Optional<Wrapped> {
+  case none
+  case some(Wrapped)
+}
+*/
+
+switch user.name {
+case .some(let value):
+  print("some - \(value)")
+case .none:
+  print("none - nil")
+}
