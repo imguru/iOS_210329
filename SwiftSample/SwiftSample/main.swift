5f25ab4db8d602f1db69bd247c2fa51e2df9965a
Chansik Yun <chansigi@gmail.com>
2021-03-29 14:44:24 +0900

    .

--- SwiftSample/SwiftSample/main.swift
+++ SwiftSample/SwiftSample/main.swift
@@ -8,9 +8,11 @@ struct User {
   
   let name: String?  // Optional<String>
   // let name: Optional<String>
+  
+  let address: String? = "Suwon"
 }
 
-let user = User(id: 0, email: "hello@gmail.com", name: nil)
+let user = User(id: 0, email: "hello@gmail.com", name: "Tom")
 
 // 1) enum을 통해서 Optional이 구현되어 있습니다.
 /*
@@ -35,3 +37,31 @@ case let value?:
 case nil:
   print("none - nil")
 }
+
+/*
+if let name = user.name {
+  if let address = user.address {
+      print("User: \(name) - \(address)")
+  }
+}
+*/
+
+// 동시에 두 개 이상의 Optional을 처리하는 것도 가능합니다.
+// - Optional Binding
+if let name = user.name,
+   let address = user.address {
+  print("User: \(name) - \(address)")
+}
+
+if let _ = user.name {
+  print("이름이 입력되어 있습니다")
+}
+
+if user.name != nil {
+  print("이름이 입력되어 있습니다")
+}
+
+if let name = user.name, !name.isEmpty {
+  print("이름이 제대로 입력되어 있습니다")
+}
+
