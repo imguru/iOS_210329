cc57539e6279b0fdf58cfc9f097e819943a89f9c
Chansik Yun <chansigi@gmail.com>
2021-03-29 10:02:08 +0900

    .

--- SwiftSample/SwiftSample/main.swift
+++ SwiftSample/SwiftSample/main.swift
@@ -7,17 +7,12 @@ import Foundation
 //  2. 채팅 참가 메세지
 //  3. 채팅 탈퇴 메세지
 
-
-
-
-
-
 #if false
 struct Message {
   let userId: Int
   let contents: String?
   let date: Date
-  
+
   let hasJoined: Bool
   let hasLeft: Bool
 }
@@ -38,23 +33,45 @@ enum Message {
   case text(userId: Int, contents: String, date: Date)
   case join(userId: Int, date: Date)
   case leave(userId: Int, date: Date)
-  
+
   // case xxx
 }
 
-let joinMessage: Message = Message.join(userId: 1, date: Date())
+let joinMessage = Message.join(userId: 1, date: Date())
 let textMessage = Message.text(userId: 1, contents: "Hello", date: Date())
 let leaveMessage: Message = .leave(userId: 1, date: Date())
 
 func logMessage(message: Message) {
   // 모든 경우를 처리하는 형태로 만들어 놓는 것이 좋다.
   // => 새로운 case가 추가되었을 경우, 컴파일 오류가 발생한다.
+  #if false
   switch message {
   case .text(userId: let userId, contents: let contents, date: let date):
     print("Text - \(userId) / \(contents) / \(date)")
   case .join(userId: let userId, date: let date):
     print("Join - \(userId) / \(date)")
   case .leave(userId: let userId, date: let date):
+    print("Leave - \(userId) / \(date)")
+  }
+  #endif
+
+  #if false
+  switch message {
+  case .text(let userId, let contents, let date):
+    print("Text - \(userId) / \(contents) / \(date)")
+  case .join(let userId, let date):
+    print("Join - \(userId) / \(date)")
+  case .leave(let userId, let date):
+    print("Leave - \(userId) / \(date)")
+  }
+  #endif
+  
+  switch message {
+  case let .text(userId, contents, date):
+    print("Text - \(userId) / \(contents) / \(date)")
+  case let .join(userId, date):
+    print("Join - \(userId) / \(date)")
+  case let .leave(userId, date):
     print("Leave - \(userId) / \(date)")
   }
 }
