2dad3614d7ab261f5ad733e5cbdaddeb8c144f33
Chansik Yun <chansigi@gmail.com>
2021-03-30 14:44:35 +0900

    .

--- SwiftSample/SwiftSample/main.swift
+++ SwiftSample/SwiftSample/main.swift
@@ -29,7 +29,8 @@ printValues(arr)
 #endif
 
 // 제약 사항이 여러 개 존재할 경우,
-//  T: Comparable & Hashable
+//  1) <T: Comparable & Hashable>
+//  2) where T: Comparable & Hashable
 #if false
 func printValues<T: Comparable & Hashable>(_ values: [T]) {
   print(values.sorted()) // Comparable
@@ -43,6 +44,7 @@ func printValues<T: Comparable & Hashable>(_ values: [T]) {
   print(result)
 }
 #endif
+
 func printValues<T>(_ values: [T])
   where T: Comparable & Hashable
 {
@@ -68,3 +70,46 @@ let arr = [
   "me",
 ]
 printValues(arr)
+
+struct User {
+  let name: String
+  let age: Int
+}
+
+extension User : Comparable {
+  static func < (lhs: User, rhs: User) -> Bool {
+    return lhs.name < rhs.name
+  }
+}
+
+// Java: equals / hashCode는 반드시 동시에 제공되어야 한다.
+//  - hashCode
+//  - equals
+
+// => Swift는 Hashbale에서 Equtable을 상속하는 형태로 구현되어 있습니다.
+
+extension User : Hashable {
+  // Equtable
+  static func == (lhs: User, rhs: User) -> Bool {
+    return lhs.name == rhs.name && lhs.age == rhs.age
+  }
+  
+  // Hashable
+  func hash(into hasher: inout Hasher) {
+    hasher.combine(name)
+    hasher.combine(age)
+  }
+}
+
+let arr2 = [
+  User(name: "Tom", age: 42),
+  User(name: "Bob", age: 100),
+  User(name: "Tom", age: 42),
+  User(name: "Bob", age: 100),
+  User(name: "Tom", age: 42),
+  User(name: "Bob", age: 100),
+  User(name: "Tom", age: 42),
+  User(name: "Bob", age: 100),
+]
+printValues(arr2)
+
