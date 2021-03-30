fcb903d644d3dd9aca86b2033678fef58eda5f04
Chansik Yun <chansigi@gmail.com>
2021-03-30 17:14:09 +0900

    .

---
+++ SwiftSample/SwiftSample/9_Generic6.swift
@@ -0,0 +1,186 @@
+import Foundation
+
+// - 프로토콜은 제네릭에 의해 추론될 수 없다.
+// - 프로토콜은 추론되는 타입이 해당 프로토콜을 만족하는 목적으로만 사용된다.
+
+#if false
+protocol Job {
+  func start(input: String) -> Bool
+}
+
+class MailJob: Job {
+  @discardableResult
+  func start(input: String) -> Bool {
+    print("MailJob start: \(input)")
+    return true
+  }
+}
+
+let job = MailJob()
+job.start(input: "hello@gmail.com")
+
+// _ = job.start(input: "hello@gmail.com")
+#endif
+
+// Protocols do not allow generic parameters;
+// use associated types instead
+// => 프로토콜은 제네릭을 타입 인자를 허용하지 않습니다.
+//    PAT(Protocol Associated Type, 프로토콜 연관 타입)를 사용해야 합니다.
+#if false
+// protocol Job<Input, Output> {
+//   func start(input: Input) -> Output
+// }
+#endif
+
+// class / struct / enum
+protocol Job {
+  associatedtype Input
+  associatedtype Output
+
+  func start(input: Input) -> Output
+}
+
+class MailJob: Job {
+  // typealias Input = String
+  // typealias Output = Bool
+
+  @discardableResult
+  func start(input: String) -> Bool {
+    print("MailJob start: \(input)")
+    return true
+  }
+}
+
+let job = MailJob()
+job.start(input: "hello@gmail.com")
+
+struct DirRemover: Job {
+  typealias Input = URL
+  typealias Output = [String]
+
+  func start(input: URL) -> [String] {
+    do {
+      var results = [String]()
+      let fileManager = FileManager.default
+      let fileUrls = try fileManager.contentsOfDirectory(at: input, includingPropertiesForKeys: nil)
+
+      for file in fileUrls {
+        try fileManager.removeItem(at: file)
+        results.append(file.absoluteString)
+      }
+
+      return results
+    } catch {
+      print("Error - \(error)")
+      return []
+    }
+  }
+}
+
+enum SendJob {
+  case mail(email: String)
+  case sms(phone: String)
+}
+
+extension SendJob: Job {
+  typealias Input = String
+  typealias Output = Bool
+
+  func start(input: String) -> Bool {
+    switch self {
+    case .mail(let email):
+      print("Send Mail - \(email)")
+    case .sms(let phone):
+      print("Send SMS - \(phone)")
+    }
+    return true
+  }
+}
+
+// --------
+// PAT 기반의 프로토콜을 사용하면, 런타임 다형성을 위한 코드를 사용할 수 없습니다.
+// => 반드시 컴파일 타임 다형성을 통해 코드를 작성해야 합니다.
+//  Protocol 'Job' can only be used as a generic constraint
+// --------
+#if false
+let jobs: [Job] = [
+  MailJob(),
+  SendJob.mail(email: "hello@gmail.com"),
+]
+
+func runJob(job: Job) {
+  // ...
+}
+#endif
+
+func runJob<J: Job>(job: J, inputs: [J.Input]) {
+  for input in inputs {
+    _ = job.start(input: input)
+  }
+}
+
+let emails = [
+  "hello1@gmail.com",
+  "hello2@gmail.com",
+  "hello3@gmail.com",
+]
+
+runJob(job: MailJob(), inputs: emails)
+runJob(job: SendJob.sms(phone: "000-111-2222"), inputs: emails)
+
+let dirs: [URL] = [
+  // URL(fileURLWithPath: "/Users/ourguide/Desktop/aaa") // 주의! 진짜 지워집니다.
+]
+runJob(job: DirRemover(), inputs: dirs)
+
+// Protocol 'Equatable' can only be used as a generic constraint
+//  => Equatable(PAT)
+//     1) associatedtype
+//     2) Self
+#if false
+func areAllEquals<T: Equatable>(value: T, values: [T]) -> Bool {
+  for e in values {
+    if e != value {
+      return false
+    }
+  }
+  return true
+}
+#endif
+
+struct User {
+  let email: String
+  let sms: String
+}
+
+struct UserMailJob: Job {
+  typealias Input = User
+  typealias Output = Bool
+  
+  func start(input: User) -> Bool {
+    print("UserMailJob - \(input.email)")
+    return true
+  }
+}
+
+let users = [
+  User(email: "hello1@gmail.com", sms: ""),
+  User(email: "hello2@gmail.com", sms: ""),
+  User(email: "hello3@gmail.com", sms: ""),
+]
+runJob(job: UserMailJob(), inputs: users)
+
+
+// 만약 Input Type이 User인 경우, 로직이 다르다.
+//  => C++ 부분 전문화
+//  => where J.Input == User
+func runJob<J: Job>(job: J, inputs: [J.Input]) where J.Input == User {
+  for input in inputs {
+    print("User - \(input.email)")  // 컴파일러는 where 구문을 통해 J.Input이 User 타입임을 알 수 있습니다.
+    _ = job.start(input: input)
+  }
+}
+
+
+
+
