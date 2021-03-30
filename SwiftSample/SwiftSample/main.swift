f7747ee933323e1f93e4012fc26e9c25c1394b58
Chansik Yun <chansigi@gmail.com>
2021-03-30 16:37:22 +0900

    .

--- SwiftSample/SwiftSample/main.swift
+++ SwiftSample/SwiftSample/main.swift
@@ -32,17 +32,18 @@ job.start(input: "hello@gmail.com")
 // }
 #endif
 
+// class / struct / enum
 protocol Job {
   associatedtype Input
   associatedtype Output
-  
+
   func start(input: Input) -> Output
 }
 
 class MailJob: Job {
   // typealias Input = String
   // typealias Output = Bool
-  
+
   @discardableResult
   func start(input: String) -> Bool {
     print("MailJob start: \(input)")
@@ -53,11 +54,45 @@ class MailJob: Job {
 let job = MailJob()
 job.start(input: "hello@gmail.com")
 
-class DirRemover : Job {
+struct DirRemover: Job {
   typealias Input = URL
   typealias Output = [String]
-  
+
   func start(input: URL) -> [String] {
+    do {
+      var results = [String]()
+      let fileManager = FileManager.default
+      let fileUrls = try fileManager.contentsOfDirectory(at: input, includingPropertiesForKeys: nil)
+
+      for file in fileUrls {
+        try fileManager.removeItem(at: file)
+        results.append(file.absoluteString)
+      }
+
+      return results
+    } catch {
+      print("Error - \(error)")
       return []
+    }
+  }
+}
+
+enum SendJob {
+  case mail(email: String)
+  case sms(phone: String)
+}
+
+extension SendJob: Job {
+  typealias Input = String
+  typealias Output = Bool
+
+  func start(input: String) -> Bool {
+    switch self {
+    case .mail(let email):
+      print("Send Mail - \(email)")
+    case .sms(let phone):
+      print("Send SMS - \(phone)")
+    }
+    return true
   }
 }
