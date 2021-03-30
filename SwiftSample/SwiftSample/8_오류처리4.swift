ec6155676b005de307423ddf876afc45465349bf
Chansik Yun <chansigi@gmail.com>
2021-03-30 12:03:36 +0900

    .

---
+++ SwiftSample/SwiftSample/8_오류처리4.swift
@@ -0,0 +1,108 @@
+import Foundation
+
+enum ApiError: Error {
+  case network(String)
+  case client(String)
+  case server
+}
+
+struct User {
+  let login: String
+  let company: String
+}
+
+func getGithubUser(login: String) throws -> User {
+  if login == "root" {
+    throw ApiError.client("Invalid login name").nsError
+  }
+
+  return User(login: login, company: "LG")
+}
+
+do {
+  let user = try getGithubUser(login: "root")
+  print(user)
+} catch {
+  // print(error)
+  ErrorHandler.default.handle(error)
+}
+
+// 오류 처리를 중앙 집중적으로 관리하면, 오류 처리의 중복된 코드를 한곳에 모아서 관리할 수 있습니다.
+// Singleton
+//   - `default`
+//
+struct ErrorHandler {
+  static let `default` = ErrorHandler()
+  
+  func handle(_ error: Error) {
+    print(error)
+  }
+}
+
+
+
+extension ApiError: LocalizedError {
+  var errorDescription: String? {
+    switch self {
+    case .network(let message):
+      return "errorDescription: network(\(message))"
+    case .client(let message):
+      return "errorDescription: client(\(message))"
+    case .server:
+      return "errorDescription: server"
+    }
+  }
+
+  var failureReason: String? {
+    switch self {
+    case .network:
+      return "failureReason: 네트워크 오류 입니다"
+    case .client:
+      return "failureReason: 클라이언트가 잘못했습니다."
+    case .server:
+      return "failureReason: 서버가 잘못했습니다."
+    }
+  }
+
+  var recoverySuggestion: String? {
+    return "재부팅하세요"
+  }
+}
+
+extension ApiError: CustomNSError {
+  static var errorDomain: String {
+    return "ApiError"
+  }
+
+  var errorCode: Int {
+    switch self {
+    case .network:
+      return 100
+    case .client:
+      return 400
+    case .server:
+      return 500
+    }
+  }
+
+  var errorUserInfo: [String: Any] {
+    return [
+      NSLocalizedDescriptionKey: errorDescription ?? "",
+      NSLocalizedFailureErrorKey: failureReason ?? "",
+      NSLocalizedRecoverySuggestionErrorKey: recoverySuggestion ?? ""
+    ]
+  }
+}
+
+// -----
+extension NSError {
+  convenience init(apiError: ApiError) {
+    self.init(domain: ApiError.errorDomain, code: apiError.errorCode, userInfo: apiError.errorUserInfo)
+  }
+}
+
+extension ApiError {
+  var nsError: NSError {
+    return NSError(apiError: self)
+  }
+}
--- SwiftSample/SwiftSample/main.swift
+++ SwiftSample/SwiftSample/main.swift
@@ -1,108 +1,79 @@
 import Foundation
 
-enum ApiError: Error {
-  case network(String)
-  case client(String)
-  case server
+enum UserError: Error {
+  case emptyValueNotAllowed
+  case invalidEmail
 }
 
 struct User {
-  let login: String
-  let company: String
+  let email: String
+  
+  // 타입을 생성할 때 유효성을 미리 검증할 수 있다면, 이후의 오류 처리를 줄일 수 있습니다.
+  // - 실패의 원인이 2개 이상인 경우, 오류(예외)를 던지는 것이 좋습니다.
+  #if true
+  init(email: String) throws {
+    if email.isEmpty {
+      throw UserError.emptyValueNotAllowed
+    }
+    
+    let pattern = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+    if email.range(of: pattern, options: .regularExpression, range: nil, locale: nil) == nil {
+      throw UserError.invalidEmail
+    }
+    
+    self.email = email
+  }
+  #endif
+  
+  // 오류의 원인이 1개 라면, 예외 보다는 Optional이 편리합니다.
+  #if false
+  init?(email: String) {
+    let pattern = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+    if email.range(of: pattern, options: .regularExpression, range: nil, locale: nil) == nil {
+      return nil  // !!!
+    }
+    
+    self.email = email
+  }
+  #endif
 }
 
-func getGithubUser(login: String) throws -> User {
-  if login == "root" {
-    throw ApiError.client("Invalid login name").nsError
-  }
+// init?()
+#if false
+if let user = User(email: "hello@") {
+  print(user)
+} else {
+  print("생성 실패")
+}
+#endif
 
-  return User(login: login, company: "LG")
+// throws
+// 1. try  - 외부로 전파(do - catch)
+// 2. try! - 오류(예외)발생시 프로그램 종료
+// 3. try?
+if let user = try? User(email: "hello@") {
+  print(user)
+} else {
+  print("생성 실패")
+}
+
+
+
+
+
+// init(..) throws
+#if false
+do {
+  let user = try User(email: "")
+  print(user)
+} catch {
+  print(error)
 }
 
 do {
-  let user = try getGithubUser(login: "root")
+  let user = try User(email: "@a.com")
   print(user)
 } catch {
-  // print(error)
-  ErrorHandler.default.handle(error)
+  print(error)
 }
-
-// 오류 처리를 중앙 집중적으로 관리하면, 오류 처리의 중복된 코드를 한곳에 모아서 관리할 수 있습니다.
-// Singleton
-//   - `default`
-//
-struct ErrorHandler {
-  static let `default` = ErrorHandler()
-  
-  func handle(_ error: Error) {
-    print(error)
-  }
-}
-
-
-
-extension ApiError: LocalizedError {
-  var errorDescription: String? {
-    switch self {
-    case .network(let message):
-      return "errorDescription: network(\(message))"
-    case .client(let message):
-      return "errorDescription: client(\(message))"
-    case .server:
-      return "errorDescription: server"
-    }
-  }
-
-  var failureReason: String? {
-    switch self {
-    case .network:
-      return "failureReason: 네트워크 오류 입니다"
-    case .client:
-      return "failureReason: 클라이언트가 잘못했습니다."
-    case .server:
-      return "failureReason: 서버가 잘못했습니다."
-    }
-  }
-
-  var recoverySuggestion: String? {
-    return "재부팅하세요"
-  }
-}
-
-extension ApiError: CustomNSError {
-  static var errorDomain: String {
-    return "ApiError"
-  }
-
-  var errorCode: Int {
-    switch self {
-    case .network:
-      return 100
-    case .client:
-      return 400
-    case .server:
-      return 500
-    }
-  }
-
-  var errorUserInfo: [String: Any] {
-    return [
-      NSLocalizedDescriptionKey: errorDescription ?? "",
-      NSLocalizedFailureErrorKey: failureReason ?? "",
-      NSLocalizedRecoverySuggestionErrorKey: recoverySuggestion ?? ""
-    ]
-  }
-}
-
-// -----
-extension NSError {
-  convenience init(apiError: ApiError) {
-    self.init(domain: ApiError.errorDomain, code: apiError.errorCode, userInfo: apiError.errorUserInfo)
-  }
-}
-
-extension ApiError {
-  var nsError: NSError {
-    return NSError(apiError: self)
-  }
-}
+#endif
