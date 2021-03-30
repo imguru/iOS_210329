32178ae20aad436dc30d5c0f55421c6777088c04
Chansik Yun <chansigi@gmail.com>
2021-03-30 10:49:20 +0900

    .

--- SwiftSample/SwiftSample/main.swift
+++ SwiftSample/SwiftSample/main.swift
@@ -21,7 +21,7 @@ struct Log {
     }
   }
   #endif
-  
+
   // 2. 연산이 완료된 이후에 객체의 상태가 변경되도록 해야 한다.
   //   => 복사본을 이용하는 것이 좋습니다.
   mutating func append(messages: [String]) throws {
@@ -37,7 +37,6 @@ struct Log {
     }
     values = temp
   }
-  
 }
 
 // 1. 오류 가능성이 있는 연산이 수행되었을 때,
@@ -56,3 +55,39 @@ do {
 }
 
 print(log)
+
+func writeToFiles(data: [URL: String]) throws {
+  var completed = [URL]()
+  
+  // defer 블록에서는 외부로 예외를 전파할 수 없다.
+  // try!: 예외가 발생하였을 경우 비정상 종료합니다.
+  defer {
+    if completed.count != data.count {  // 오류가 발생하였다
+      print("예외가 발생하였습니다.")
+      
+      for url in completed {
+        do {
+          try FileManager.default.removeItem(at: url)
+        } catch {
+          print("복구할 수 없는 오류가 발생하였습니다.")
+        }
+      }
+    }
+  }
+  
+  for (url, contents) in data {
+    try contents.write(to: url, atomically: true, encoding: .utf8)
+    completed.append(url)
+  }
+  
+}
+
+do {
+  try writeToFiles(data: [
+    URL(fileURLWithPath: "/Users/ourguide/Desktop/a.txt"): "hello world",
+    URL(fileURLWithPath: "~/b.txt"): "hello world",
+    URL(fileURLWithPath: "/Users/ourguide/Desktop/c.txt"): "hello world",
+  ])
+} catch {
+  print(error)
+}
