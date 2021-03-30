4f7e60326f679387e4f8e8299f7b30b83ee18caa
Chansik Yun <chansigi@gmail.com>
2021-03-30 13:54:56 +0900

    .

--- SwiftSample/SwiftSample/main.swift
+++ SwiftSample/SwiftSample/main.swift
@@ -7,6 +7,7 @@ import Foundation
 //            "다형성"
 //  - 제네릭을 이용하면 타입은 다르지만, 동일한 로직을 갖고 있는 함수에 대해서 중복을 제거할 수 있다.
 //  - 한번만 만들면 되고, 아직 작성되지 않은 다양한 타입에 대해서도 대응이 가능하다.
+//  - Any를 사용하지 않기 때문에, 실행 시간에 다운 캐스트 같은 불필요한 동작이 필요하지 않습니다.
 
 // 'Generic'을 구현하는 방식
 //  => C++, C#, Java, Kotlin
@@ -41,3 +42,34 @@ let arr2 = ["Apple", "Banana", "Orange"]
 let result2 = firstAndLast(arr2)
 
 print("\(result2.0) / \(result2.1)")
+
+struct User {
+  let name: String
+}
+
+let arr3 = [
+  User(name: "Tom"),
+  User(name: "Bob"),
+]
+
+let result3 = firstAndLast(arr3)
+print("\(result3.0) / \(result3.1)")
+
+
+
+
+#if false
+// Java에서는 타입 인자는 Object 타입으로 컴파일 됩니다.
+// => Generic에 원시 타입(int, double, char)을 저장할 수 없습니다.
+// => Boxing / Wrapper class(Integer, Double, Char)
+
+// ArrayList<Integer> arr = new ArrayList<>();
+// arr.add(10);
+// arr.add(20);
+// add.add("hello");  // Compile error O
+
+// ArrayList arr = new ArrayList();
+// arr.add(Integer.valueOf(10));
+// arr.add(Integer.valueOf(20));
+// add.add("hello");  // Compile error X
+#endif
