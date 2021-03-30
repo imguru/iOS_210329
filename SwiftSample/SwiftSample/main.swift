b77b6c5313ada17a2639dffda769fb76335d30c0
Chansik Yun <chansigi@gmail.com>
2021-03-30 14:58:51 +0900

    .

--- SwiftSample/SwiftSample/main.swift
+++ SwiftSample/SwiftSample/main.swift
@@ -115,9 +115,19 @@ let arr2 = [
 ]
 printValues(arr2)
 
+
+//----------------------------------------
+let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for number in numbers {
+  if number % 2 == 0 {
+    print(number)
+  }
+}
 #if false
 let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 for number in numbers where number % 2 == 0 {
     print(number)
 }
 #endif
+
+
