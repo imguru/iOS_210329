0af578ca086ed2e9b434da96a1fc7c8e8e9379b8
Chansik Yun <chansigi@gmail.com>
2021-03-31 15:04:52 +0900

    .

--- SwiftSample/SwiftSample/main.swift
+++ SwiftSample/SwiftSample/main.swift
@@ -14,9 +14,20 @@ let suits = ["Hearts", "Clubs", "Diamonds", "Spades"]
 let faces = ["2", "3", "4" ]
 // let faces = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 
-// Array<String> -> map -> Array<Array<Tuple>>
+// Array<String> -> map        -> Array<Array<Tuple>>
 
-let deck = suits.map { suit in  
+
+#if false
+let deck = suits.map { suit in
+  faces.map { face in
+    (suit, face)
+  }
+}
+#endif
+
+//                 flatten
+// Array<String> -> flatMap    -> Array<Tuple>
+let deck = suits.flatMap { suit in
   faces.map { face in
     (suit, face)
   }
