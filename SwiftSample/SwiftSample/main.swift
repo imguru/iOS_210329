09b24403e342b5b1a5f768f385c13a2629c2d29c
Chansik Yun <chansigi@gmail.com>
2021-03-31 15:10:26 +0900

    .

--- SwiftSample/SwiftSample/main.swift
+++ SwiftSample/SwiftSample/main.swift
@@ -11,8 +11,8 @@ import Foundation
 //      A?  -> map ->  B?
 
 let suits = ["Hearts", "Clubs", "Diamonds", "Spades"]
-let faces = ["2", "3", "4" ]
-// let faces = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+// let faces = ["2", "3", "4" ]
+let faces = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 
 // Array<String> -> map        -> Array<Array<Tuple>>
 
@@ -33,4 +33,19 @@ let deck = suits.flatMap { suit in
   }
 }
 
-print(deck)
+// print(deck)
+
+let info = [
+  "url": "https://api.github.com/users"
+]
+
+let path: String? = info["url"]
+// String -> URL
+
+// Optional<String> -> map -> Optional<Optional<URL>>
+let url = path.map { URL(string: $0) }
+print(type(of: url))
+
+let url2 = path.flatMap { URL(string: $0) }
+print(type(of: url2))
+
