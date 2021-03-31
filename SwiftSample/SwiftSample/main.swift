68bf27a6e0698801cc91f634015c34aa613ca269
Chansik Yun <chansigi@gmail.com>
2021-03-31 16:22:06 +0900

    .

--- SwiftSample/SwiftSample/main.swift
+++ SwiftSample/SwiftSample/main.swift
@@ -1,7 +1,7 @@
 
 import Foundation
 
-let searchUrl = "https://api.github.com/search/users?q={keyword}"
+let searchUrl = "https://api.github.com/search/users?q="
 
 enum SearchResultError: Error {
   case invalidQuery(String)
@@ -9,13 +9,25 @@ enum SearchResultError: Error {
   case networkError(NetworkError)
 }
 
+typealias JSON = [String: Any]
 
+func searchUsers(q: String, completion: @escaping (Result<JSON, SearchResultError>) -> Void) {
+  let encodedQuery = q.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) // String?
 
+  // String? -> map -> String?
+  let path = encodedQuery.map {
+    searchUrl + $0
+  }
 
+  guard let url = path.flatMap(URL.init) else {
+    completion(.failure(.invalidQuery(q)))
+    return
+  }
 
-
-
-
+  getJSON(with: url) { result in
+    // ...
+  }
+}
 
 // ----------------------------
 // 이전 소스 참조
