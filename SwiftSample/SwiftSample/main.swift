0e1119b0be59dad5d21586be794febd27a629a3a
Chansik Yun <chansigi@gmail.com>
2021-03-31 15:17:24 +0900

    .

--- SwiftSample/SwiftSample/main.swift
+++ SwiftSample/SwiftSample/main.swift
@@ -16,7 +16,6 @@ let faces = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 
 // Array<String> -> map        -> Array<Array<Tuple>>
 
-
 #if false
 let deck = suits.map { suit in
   faces.map { face in
@@ -34,9 +33,8 @@ let deck = suits.flatMap { suit in
 }
 
 // print(deck)
-
 let info = [
-  "url": "https://api.github.com/users"
+  "url": "https://api.github.com/users",
 ]
 
 let path: String? = info["url"]
@@ -48,4 +46,33 @@ print(type(of: url))
 
 let url2 = path.flatMap { URL(string: $0) }
 print(type(of: url2))
+
+// --------------
+
+let strings = [
+  "https://naver.com",
+  "https://daum.net",
+  " ",
+  "https://facebook.com",
+]
+
+// compactMap
+let urls = strings.compactMap {
+  URL(string: $0) // init?(...)
+}
+print(urls)
+
+
+#if false
+let urls: [URL] = strings.map {
+  URL(string: $0) // init?(...)
+}.filter {
+  $0 != nil
+}.map {
+  $0!
+}
+print(urls)
+#endif
+
+
 
