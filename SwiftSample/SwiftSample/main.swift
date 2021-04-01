b2644858433805a3183d4df8507223c0dfe4c360
Chansik Yun <chansigi@gmail.com>
2021-04-01 11:06:55 +0900

    .

--- SwiftSample/SwiftSample/main.swift
+++ SwiftSample/SwiftSample/main.swift
@@ -11,6 +11,7 @@ extension UIViewController {
     print("UIViewController 이벤트 추적")
   }
 }
+
 // 문제점: 원하지 않는 사용자들도, UIViewController에 track 기능이 추가됩니다.
 //  => 함부로 추가하지 않는 것이 좋습니다.
 
@@ -29,11 +30,71 @@ extension AnalyticsProtocol where Self: UIViewController {
   }
 }
 
+#if false
 let controller = UIViewController()
 // track의 기능을 이용하고 싶다면,
-extension UIViewController: AnalyticsProtocol {}  // !!!
+extension UIViewController: AnalyticsProtocol {} // !!!
 
 controller.track(event: "hello", parameters: [:])
+#endif
+// ----------------------------------------------------------------
 
+struct User : Hashable {
+  let name: String
+}
+#if false
 
+let arr2 = [ User(name: "Tom") ]
+let result2 = arr2.unique()
+print(result2)
+#endif
 
+let arr = [User(name: "Tom"), User(name: "Bob")]
+let result = arr.unique()
+print(result)
+
+let arr3: Set<Int> = [ 1, 2, 3, 4, 4, 3, 2, 1 ]
+let result3 = arr3.unique()
+print(result3)
+
+extension Collection where Element: Equatable {
+  // O(N)
+  func unique() -> [Element] {
+    print("Equatable 버전")
+    var uniqueValues = [Element]()
+
+    for element in self {
+      if !uniqueValues.contains(element) { // Equtable
+        uniqueValues.append(element)
+      }
+    }
+
+    return uniqueValues
+  }
+}
+
+extension Collection where Element: Hashable {
+  // O(1)
+  func unique() -> [Element] {
+    print("Hashable 버전")
+    return Array(self)
+  }
+}
+
+struct Article {
+  let viewCount: Int
+}
+
+let a1 = Article(viewCount: 30)
+let a2 = Article(viewCount: 300)
+
+let articles = [ a1, a2 ]
+print(articles.totalViewCount)
+
+extension Collection where Element == Article {
+  var totalViewCount: Int {
+    return reduce(0) { (result, element) -> Int in
+      return result + element.viewCount
+    }
+  }
+}
