b7eaa96c6d49bdfe0f1319e135b139469742a304
Chansik Yun <chansigi@gmail.com>
2021-04-01 15:11:22 +0900

    .

--- SwiftSample/SwiftSample/main.swift
+++ SwiftSample/SwiftSample/main.swift
@@ -69,3 +69,12 @@ extension Validator {
 
 let v3 = minValidator.combine(maxValidator)
 print(v3.validate("hellllo"))
+
+//------------------------------------------------------------
+//  가벼운 다형성                  ->       enum
+
+//  다양한 타입의 동작이 필요하다.     ->       Generic struct / class
+//  정책의 변화가 필요한 타입이 필요하다         클로저를 활용해서 만드는 방법
+
+// 복잡한 다형성 / 기본 구현         ->       protocol
+
