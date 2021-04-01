d5c0bb0bca10533df07e672b7f6dd66c2cfa48dd
Chansik Yun <chansigi@gmail.com>
2021-04-01 16:34:24 +0900

    .

--- SampleApp/SampleApp/ViewController.swift
+++ SampleApp/SampleApp/ViewController.swift
@@ -1,17 +1,26 @@
 
 import UIKit
 
+let IMAGE_URL = URL(string: "https://picsum.photos/1024/1024")!
+
 class ViewController: UIViewController {
-  
   @IBOutlet var imageView: UIImageView!
   @IBOutlet var timeLabel: UILabel!
   
-  
-  @IBAction func onCancel(_ sender: UIButton) {
-  }
-  
+  // 1. 동기 버전
   @IBAction func onLoad(_ sender: UIButton) {
+    guard let data = try? Data(contentsOf: IMAGE_URL) else {
+      return
+    }
+    
+    guard let image = UIImage(data: data) else {
+      return
+    }
+    
+    imageView.image = image
   }
+  
+  @IBAction func onCancel(_ sender: UIButton) {}
   
   override func viewDidLoad() {
     super.viewDidLoad()
