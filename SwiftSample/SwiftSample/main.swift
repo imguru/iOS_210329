15da0a86f531637a47909c26a7ae499376649fcd
Chansik Yun <chansigi@gmail.com>
2021-04-01 14:51:49 +0900

    .

--- SwiftSample/SwiftSample/main.swift
+++ SwiftSample/SwiftSample/main.swift
@@ -1,8 +1,6 @@
 
 import Foundation
 
-
-
 // Protocol 'Game' can only be used as a generic constraint because it has Self or associated type requirements
 // => Hashable이 상속하는 Equatable의 프로토콜이 내부적으로 Self를 사용하고 있기 때문에
 //    런타임 다형성을 사용할 수 없습니다.
@@ -23,7 +21,6 @@ struct PCGame: Game {
   }
 }
 
-
 let numberOfPlayers: [Game: Int] = [
   VideoGame(): 40,
   PCGame(): 100,
@@ -33,7 +30,7 @@ let numberOfPlayers: [Game: Int] = [
 #if false
 let arr: [Game] = [
   VideoGame(),
-  PCGame()
+  PCGame(),
 ]
 
 for e in arr {
@@ -44,7 +41,7 @@ for e in arr {
 // 해결 방법
 // 1) enum을 사용하자
 //   => enum은 struct와 동일하게 '조건부 순응'을 제공합니다.
-
+#if false
 enum Game: Hashable {
   case video(VideoGame)
   case pc(PCGame)
@@ -65,4 +62,56 @@ struct PCGame: Hashable {
 let numberOfPlayers: [Game: Int] = [
   .video(VideoGame()): 40,
   .pc(PCGame()): 100,
+]
+#endif
+
+// 2. Boxing을 이용하면 문제를 해결할 수 있습니다.
+protocol Game: Hashable {
+  func start()
+}
+
+struct VideoGame: Game {
+  func start() {
+    print("VideoGame start")
+  }
+}
+
+struct PCGame: Game {
+  func start() {
+    print("PCGame start")
+  }
+}
+
+struct AnyGame: Game {
+  private let _start: () -> Void // start
+  private let _hashable: AnyHashable
+
+  func start() {
+    _start()
+  }
+
+  init<T: Game>(_ game: T) {
+    _hashable = game
+    _start = game.start
+  }
+
+  // AnyHashable
+  static func == (lhs: AnyGame, rhs: AnyGame) -> Bool {
+    return lhs._hashable == rhs._hashable
+  }
+
+  func hash(into hasher: inout Hasher) {
+    _hashable.hash(into: &hasher)
+  }
+}
+
+let pcGame = AnyGame(PCGame())
+pcGame.start()
+
+let videoGame = AnyGame(VideoGame())
+videoGame.start()
+
+let numberOfPlayers: [AnyGame: Int] = [
+  videoGame: 40,
+  pcGame: 100,
 ]
