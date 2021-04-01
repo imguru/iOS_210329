9c1e5d0bcee4ca26e46d62aac0c4d02ae66e8b2c
Chansik Yun <chansigi@gmail.com>
2021-04-01 16:10:02 +0900

    .

--- SampleApp/SampleApp/AppDelegate.swift
+++ SampleApp/SampleApp/AppDelegate.swift
@@ -1,6 +1,16 @@
 
 import UIKit
 
+// ObjC / Swift 라이브러리 의존성 관리
+// - brew install cocoapods(macOS package manager)
+// - sudo gem install cocopods(ruby)
+// 1. Cocoapods - *
+// -------------
+// - Swift
+// 2. Carthage
+// 3. Swift Package Manager(Apple)
+
+
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
   var window: UIWindow?
