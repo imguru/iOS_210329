e02b81a8203a33b81729daf9c81f01fab20af0ff
Chansik Yun <chansigi@gmail.com>
2021-04-02 15:36:59 +0900

    .

--- SampleApp/SampleApp/ViewController3.swift
+++ SampleApp/SampleApp/ViewController3.swift
@@ -1,15 +1,38 @@
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController3: UIViewController {
   @IBOutlet var emailField: UITextField!
   @IBOutlet var passwordField: UITextField!
 
   @IBOutlet var loginButton: UIButton!
+  @IBOutlet var bottomMargin: NSLayoutConstraint!
 
+  let disposeBag = DisposeBag()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
 
     // Do any additional setup after loading the view.
+    NotificationCenter.default.rx.notification(UIResponder.keyboardWillShowNotification)
+      .subscribe(onNext: { _ in
+          print("keyboardWillShowNotification")
+      })
+      .disposed(by: disposeBag)
+      
+    NotificationCenter.default.rx.notification(UIResponder.keyboardWillHideNotification)
+      .subscribe(onNext: { _ in
+          print("keyboardWillHideNotification")
+      })
+      .disposed(by: disposeBag)
+    
+    
   }
+  
+  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    view.endEditing(true)
+  }
+  
 }
