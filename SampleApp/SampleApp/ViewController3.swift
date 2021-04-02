d52d34a4204b8e4399e3ddcfadcefeceb9d62461
Chansik Yun <chansigi@gmail.com>
2021-04-02 15:49:25 +0900

    .

--- SampleApp/SampleApp/ViewController3.swift
+++ SampleApp/SampleApp/ViewController3.swift
@@ -1,7 +1,7 @@
 
-import UIKit
-import RxSwift
 import RxCocoa
+import RxSwift
+import UIKit
 
 class ViewController3: UIViewController {
   @IBOutlet var emailField: UITextField!
@@ -12,27 +12,49 @@ class ViewController3: UIViewController {
 
   let disposeBag = DisposeBag()
   
+  func keyboardHeight() -> Observable<CGFloat> {
+    let keyboardWillShowNotification = NotificationCenter.default.rx.notification(UIResponder.keyboardWillShowNotification)
+      .compactMap { notificaton -> CGFloat? in
+        (notificaton.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue.height
+      }
+    
+    let keyboardWillHideNotification = NotificationCenter.default.rx.notification(UIResponder.keyboardWillHideNotification)
+      .map { _ -> CGFloat in
+        0
+      }
+    
+    return Observable.merge([keyboardWillShowNotification, keyboardWillHideNotification])
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
 
+    #if false
     // Do any additional setup after loading the view.
     NotificationCenter.default.rx.notification(UIResponder.keyboardWillShowNotification)
-      .subscribe(onNext: { _ in
-          print("keyboardWillShowNotification")
+      .subscribe(onNext: { [weak self] notifiction in
+        
+        if let height = (notifiction.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue.height {
+          print("keyboardWillShowNotification: \(height)")
+          
+          self?.bottomMargin.constant = 16 + height
+        }
+        
       })
       .disposed(by: disposeBag)
       
     NotificationCenter.default.rx.notification(UIResponder.keyboardWillHideNotification)
-      .subscribe(onNext: { _ in
-          print("keyboardWillHideNotification")
+      .subscribe(onNext: { [weak self] _ in
+        print("keyboardWillHideNotification")
+        
+        self?.bottomMargin.constant = 16
+        
       })
       .disposed(by: disposeBag)
-    
-    
+    #endif
   }
   
   override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
     view.endEditing(true)
   }
-  
 }
