ccdaeaacbfe85aa419b5453e26895fb88655762b
Chansik Yun <chansigi@gmail.com>
2021-04-02 15:16:57 +0900

    .

---
+++ SampleApp/SampleApp/ViewController3.swift
@@ -0,0 +1,15 @@
+
+import UIKit
+
+class ViewController3: UIViewController {
+  @IBOutlet var emailField: UITextField!
+  @IBOutlet var passwordField: UITextField!
+
+  @IBOutlet var loginButton: UIButton!
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+}
