0077d80d942cfd3d1ddca72081925a54fea5fc4d
Chansik Yun <chansigi@gmail.com>
2021-04-02 11:58:30 +0900

    .

--- SampleApp/SampleApp/ViewController2.swift
+++ SampleApp/SampleApp/ViewController2.swift
@@ -1,4 +1,12 @@
 import UIKit
+import RxCocoa
+
+// RxSwift
+//  - RxSwift
+//  - RxCocoa: UI에서 발생하는 비동기의 이벤트를 Observable을 통해 처리할 수 있습니다.
+
+// 1. 이벤트 스트림은 onCompleted 시점에 파괴됩니다.
+
 
 class ViewController2: UIViewController {
   
@@ -10,5 +18,20 @@ class ViewController2: UIViewController {
     super.viewDidLoad()
 
     // Do any additional setup after loading the view.
+    _ = searchBar.rx.text
+      .compactMap({ text -> String? in
+        guard let text = text, text.count >= 3 else {
+          return nil
+        }
+        return text.lowercased()
+      })
+      .subscribe(onNext: { text in
+        self.errorLabel.text = text
+      }, onError: { error in
+        print("error - \(error)")
+      }, onCompleted: {
+        print("onComplete")
+      })
+    
   }
 }
