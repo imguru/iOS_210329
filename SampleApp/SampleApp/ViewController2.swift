d4b378e0803ce44ecc11fc5fd6503888d814fd87
Chansik Yun <chansigi@gmail.com>
2021-04-02 13:37:08 +0900

    .

--- SampleApp/SampleApp/ViewController2.swift
+++ SampleApp/SampleApp/ViewController2.swift
@@ -1,4 +1,5 @@
 import RxCocoa
+import RxSwift
 import UIKit
 
 // RxSwift
@@ -18,6 +19,10 @@ class ViewController2: UIViewController {
     print("~ViewController2")
   }
 
+  // DisposeBag 객체가 파괴되는 시점에 모든 것을 Bag안의 모든 disposable에 대해서
+  // dispose를 처리한다.
+  var disposeBag = DisposeBag()
+
   override func viewDidLoad() {
     super.viewDidLoad()
 
@@ -31,7 +36,7 @@ class ViewController2: UIViewController {
         return text.lowercased()
       }
       .subscribe(onNext: { text in
-         self.errorLabel.text = text
+        self.errorLabel.text = text
       }, onError: { error in
         print("error - \(error)")
       }, onCompleted: {
@@ -44,8 +49,9 @@ class ViewController2: UIViewController {
     //   - unowned를 통해 참조하는 객체가 이미 파괴되었을 경우, 잘못된 참조 오류 발생의 가능성이 있습니다.
     //     : Fatal error: Attempted to read an unowned reference but the object was already deallocated
     //   - weak: autoniling - 참조하는 객체가 이미 파괴된 경우, nil로 변경해준다.
-    
+
     // unowned / weak
+    #if false
     _ = searchBar.rx.text
       .compactMap { text -> String? in
         guard let text = text, text.count >= 3 else {
@@ -54,12 +60,12 @@ class ViewController2: UIViewController {
         return text.lowercased()
       }
       .subscribe(onNext: { [weak self] text in
-        Timer.scheduledTimer(withTimeInterval: 3, repeats: false) { timer in
+        Timer.scheduledTimer(withTimeInterval: 3, repeats: false) { _ in
           guard let self = self else {
             print("self는 nil입니다")
             return
           }
-          
+
           self.errorLabel.text = text
           // self?.errorLabel.text = text
         }
@@ -68,20 +74,61 @@ class ViewController2: UIViewController {
       }, onCompleted: {
         print("onComplete")
       })
-      // unowned
-      // [ capture list ]
-      /*
-      .subscribe(onNext: { [unowned self] text in
-        Timer.scheduledTimer(withTimeInterval: 3, repeats: false) { timer in
-          self.errorLabel.text = text
+    // unowned
+    // [ capture list ]
+    /*
+     .subscribe(onNext: { [unowned self] text in
+       Timer.scheduledTimer(withTimeInterval: 3, repeats: false) { timer in
+         self.errorLabel.text = text
+       }
+     }, onError: { error in
+       print("error - \(error)")
+     }, onCompleted: {
+       print("onComplete")
+     })
+     */
+    #endif
+
+    #if false
+    let disposable = searchBar.rx.text
+      .compactMap { text -> String? in
+        guard let text = text, text.count >= 3 else {
+          return nil
         }
+        return text.lowercased()
+      }
+      .subscribe(onNext: { text in
+        self.errorLabel.text = text
       }, onError: { error in
         print("error - \(error)")
       }, onCompleted: {
         print("onComplete")
       })
-      */
-    
-    
+
+    disposeBag.insert(disposable)
+    #endif
+
+    // 해결 방법 2. self를 참조하지 말고 사용하는 view를 참조한다.
+    searchBar.rx.text
+      .compactMap { text -> String? in
+        guard let text = text, text.count >= 3 else {
+          return nil
+        }
+        return text.lowercased()
+      }
+      .subscribe(onNext: { [errorLabel] text in
+        errorLabel?.text = text
+      }, onError: { error in
+        print("error - \(error)")
+      }, onCompleted: {
+        print("onComplete")
+      })
+      .disposed(by: disposeBag)
+  }
+
+  override func viewWillDisappear(_ animated: Bool) {
+    super.viewWillDisappear(animated)
+
+    // disposeBag = DisposeBag()
   }
 }
